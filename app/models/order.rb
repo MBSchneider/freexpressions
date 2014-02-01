41c2f24ecdd15d45c3cf64c9113c490618a3c570
MBSchneider <matts3@u.washington.edu>
2014-01-31 19:21:26 -0800

    updates order url

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,7 @@ class Order < ActiveRecord::Base
       :cmd => '_xclick',
       :invoice => id,
       :quantity => self.quantity,
-      :item_name => 'Freexpressions cord',
-      :return => 'http://localhost:3000/success',
-      :cancel_return => 'http://localhost:3000/cancel'
+      :item_name => 'Freexpressions cord'
     }
     if self.quantity >= 25
       values[:discount_amount] = 2.95
@@ -17,11 +15,16 @@ class Order < ActiveRecord::Base
 
     if Rails.env.production?
       values[:business] = ENV["PAYPAL_USR_PROD"]
+      values[:hosted_button_id] = ENV["PAYPAL_BTNID_PROD"]
+      values[:return] = 'http://freexpressions.herokuapp.com/success'
+      values[:cancel_return] = 'http://freexpressions.herokuapp.com/cancel'
       puts "Production"
       puts "https://www.paypal.com/cgi-bin/webscr?" + values.to_query
       return "https://www.paypal.com/cgi-bin/webscr?" + values.to_query
     else
       values[:business] = ENV["PAYPAL_USR"]
+      values[:return] = 'http://localhost:3000/success'
+      values[:cancel_return] = 'http://localhost:3000/cancel'
       return "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
     end
   end
